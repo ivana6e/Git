580858dd8ed1bf46aee8499e051724dcbd02c152
TW - IT - Ivana <ivana.lee@shoalter.com>
2024-08-19 10:43:44 +0800

    create git0

---
+++ git0/src/main/java/com/example/git0/Git0Application.java
@@ -0,0 +1,13 @@
+package com.example.git0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git0Application.class, args);
+	}
+
+}
---
+++ git0/src/test/java/com/example/git0/Git0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
